853c6484b6a4760a410fdf12704a1349b809b093
u2 <u2@lenta.ru>
2020-03-31 17:26:22 +0500

    Lesson 3 really done

--- myeventchannel/ecmain/main.go
+++ myeventchannel/ecmain/main.go
@@ -18,9 +18,12 @@ func main() {
 	ch2 := myeventchannel.NewChannel()
 	ch2.Subscribe(user2)
 
+	ch3 := myeventchannel.NewChannel()
+
 	pub := myeventchannel.NewPublisher()
 	pub.AddChannel("test", ch1)
 	pub.AddChannel("test2", ch2)
+	pub.AddChannel("test3", ch3)
 
 	if err := pub.Send("HELLO!", "test"); err != nil {
 		log.Fatalf("can't send: %s", err)
@@ -34,4 +37,14 @@ func main() {
 		log.Fatalf("can't send: %s", err)
 	}
 
+	rc := "test2"
+	cl, err := pub.RemoveChannel(rc)
+	if err != nil {
+		log.Fatalf("can't send: %s", err)
+	}
+	log.Printf("Channel %v removed!", rc)
+	log.Println("Remains channels:")
+	for chname := range cl {
+		log.Println(chname)
+	}
 }
